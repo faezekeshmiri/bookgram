4e1611990fc16d0fad9c7595781e1e848491dfd3
Your Name <you@example.com>
2023-05-11 18:12:44 +0330

    Add vuetify.

--- bookgram/src/main.js
+++ bookgram/src/main.js
@@ -2,4 +2,28 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import { fa } from "vuetify/iconsets/fa";
+import { aliases, mdi } from "vuetify/lib/iconsets/mdi";
+import "@mdi/font/css/materialdesignicons.css"; 
+import "@fortawesome/fontawesome-free/css/all.css";
+
+const vuetify = createVuetify({
+    icons: {
+        defaultSet: "mdi",
+        aliases,
+        sets: {
+          mdi,
+          fa,
+        },
+      },
+    components,
+    directives,
+})
+
+createApp(App).use(router).use(vuetify).mount('#app')
