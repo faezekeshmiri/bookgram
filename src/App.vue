f31b1231784dbc659624546b3158c7342366f1e9
Your Name <you@example.com>
2023-05-12 15:57:16 +0330

    Add navigation.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,30 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/authors">About</router-link>
   </nav>
   <div class="bg-surface-variant">
     <v-row no-gutters class="flex-nowrap no-gutters">
       <v-col cols="1" class="flex-grow-0 flex-shrink-0">
-        <v-app class="bg-surface-variant">
-          <side-bar></side-bar>
-        </v-app>
       </v-col>
       <v-col cols="1" style="min-width: 100px; max-width: 100%;" class="flex-grow-1 flex-shrink-0">
         <router-view/>
       </v-col>
     </v-row>
-  </div>
-  <v-footer class="bg-grey-lighten-1"></v-footer>
+  </div> -->
+  <v-app>
+    <v-tabs class="" align-tabs="center">
+      <v-tab to="/">Books</v-tab>
+      <v-tab to="/authors">Authors</v-tab>
+    </v-tabs>
+    <router-view></router-view>
+  </v-app>
 </template>
 
 <script>
-  import SideBar from './components/SideBar.vue';
 
   export default {
     components: {
-      SideBar
     },
     data () {
       return {
