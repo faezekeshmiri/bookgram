d26cce2391c83f110a306606d15b3f852581c621
Your Name <you@example.com>
2023-05-18 16:05:34 +0330

    Add book actions to store.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,20 @@
 import { defineStore } from "pinia";
 import { Author } from "./models/author";
+import { Book } from "./models/book";
 
 interface State {
   authors: Author[];
+  books: Book[];
 }
 
 const AUTHORS_KEY = "bookgram_authors";
+const BOOKS_KEY = "books";
 
 export const useStore = defineStore("bookgram", {
   state: (): State => {
     return {
-      authors: JSON.parse(localStorage.getItem(AUTHORS_KEY) || '[]') as Author[]
+      authors: JSON.parse(localStorage.getItem(AUTHORS_KEY) || '[]') as Author[],
+      books: JSON.parse(localStorage.getItem(BOOKS_KEY) || '[]') as Book[]
     };
   },
   actions: {
@@ -31,6 +35,21 @@ export const useStore = defineStore("bookgram", {
       if (index !== -1) {
         this.authors[index] = updatedAuthor;
         localStorage.setItem(AUTHORS_KEY, JSON.stringify(this.authors));
+      }
+    },
+    addBook(book: Book) {
+      this.books.push(book);
+      localStorage.setItem(BOOKS_KEY, JSON.stringify(this.books));
+    },
+    removeBook(index: number) {
+      this.books.splice(index, 1);
+      localStorage.setItem(BOOKS_KEY, JSON.stringify(this.books));
+    },
+    updateBook(updatedBook: Book) {
+      const index = this.books.findIndex((book) => book.id === updatedBook.id);
+      if (index !== -1) {
+        this.books[index] = updatedBook;
+        localStorage.setItem(BOOKS_KEY, JSON.stringify(this.books));
       }
     },
   },
