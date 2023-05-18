9f3dca4a643ff320d98631d34aed0d453cef8939
Your Name <you@example.com>
2023-05-18 19:26:52 +0330

    Add footer.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
-  <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/authors">About</router-link>
-  </nav>
-  <div class="bg-surface-variant">
-    <v-row no-gutters class="flex-nowrap no-gutters">
-      <v-col cols="1" class="flex-grow-0 flex-shrink-0">
-      </v-col>
-      <v-col cols="1" style="min-width: 100px; max-width: 100%;" class="flex-grow-1 flex-shrink-0">
-        <router-view/>
-      </v-col>
-    </v-row>
-  </div> -->
   <v-app>
     <v-tabs class="" align-tabs="center">
       <v-tab to="/">Books</v-tab>
       <v-tab to="/authors">Authors</v-tab>
     </v-tabs>
-    <router-view></router-view>
+    <v-row class="h-75">
+      <v-col>
+        
+      </v-col>
+      <v-col>
+        <router-view></router-view>
+      </v-col>
+    </v-row>
+    
+    <v-footer class="bg-green-lighten-4" style="padding: 0px !important;" >
+      <div class="px-4 py-2 text-success text-center w-100">
+        &copy; 2023 Bookgram
+      </div>
+    </v-footer>
   </v-app>
 </template>
 
@@ -33,24 +33,4 @@
   }
 </script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
